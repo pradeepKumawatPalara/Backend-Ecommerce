82bf79daceffc31e48c60d4f2777f1f038208047
pradeep <pradeeppalara@gmail.com>
2023-09-09 23:34:28 +0530

    DONE

--- services/common.js
+++ services/common.js
@@ -2,47 +2,47 @@ const passport = require('passport');
 const nodemailer = require('nodemailer');
 
 let transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false, // true for 465, false for other ports
-  auth: {
-    user: 'coderdost@gmail.com', // gmail
-    pass: process.env.MAIL_PASSWORD, // pass
-  },
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+        user: 'pradeeppalara@gmail.com', // gmail
+        pass: process.env.EMAIL_PASSWORD, // pass
+    },
 });
 
 
 exports.isAuth = (req, res, done) => {
-  return passport.authenticate('jwt');
+    return passport.authenticate('jwt');
 };
 
 exports.sanitizeUser = (user) => {
-  return { id: user.id, role: user.role };
+    return { id: user.id, role: user.role };
 };
 
-exports.cookieExtractor = function (req) {
-  let token = null;
-  if (req && req.cookies) {
-    token = req.cookies['jwt'];
-  }
-  return token;
+exports.cookieExtractor = function(req) {
+    let token = null;
+    if (req && req.cookies) {
+        token = req.cookies['jwt'];
+    }
+    return token;
 };
 
 
-exports.sendMail = async function ({to, subject, text, html}){
+exports.sendMail = async function({ to, subject, text, html }) {
     let info = await transporter.sendMail({
-        from: '"E-commerce" <coderdost@gmail.com>', // sender address
+        from: `E-commerce ${process.env.EMAIL}`, // sender address
         to,
         subject,
         text,
         html
-      });
-    return info;  
+    });
+    return info;
 }
 
-exports.invoiceTemplate = function(order){
+exports.invoiceTemplate = function(order) {
 
- return (`<!DOCTYPE html>
+        return (`<!DOCTYPE html>
 <html>
 <head>
 
